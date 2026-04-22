a3f97d69f32cda89f52a4abe3641deb1fc340df4
Radif <radifzafarian@gmail.com>
2026-04-22 14:43:06 +0700

    Pacman Can Eat Food
    - Added score UI
    - Pacman can eat food for 10pts
    - Added Health UI
    - Food only spawns to a maximum limit

--- assets/scripts/GameController.ts
+++ assets/scripts/GameController.ts
@@ -1,35 +1,78 @@
-import { _decorator, CCInteger, Component, instantiate, Node, Prefab, view } from 'cc';
+import { _decorator, CCInteger, Collider2D, Component, instantiate, Label, Node, Prefab, view } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('GameController')
 export class GameController extends Component {
-    @property({type:CCInteger})
-    private foodSpawnInterval;
+    @property({ type: CCInteger })
+    private foodSpawnInterval: number = 2;
 
-    @property({type:Prefab})
-    private foodPrefab:Prefab;
+    @property({ type: Label })
+    private scoreLabel: Label
 
-    private minX:number = -400;
-    private maxX:number = 400;
-    private minY:number = -300;
-    private maxY:number = 300;
+    @property({ type: CCInteger })
+    private maxFood: number = 10;
+
+    @property({ type: Prefab })
+    private foodPrefab: Prefab;
+
+    @property(Node)
+    public foodContainer: Node;
+
+    private currFood: number = 0;
+
+    private minX: number = -400;
+    private maxX: number = 400;
+    private minY: number = -300;
+    private maxY: number = 300;
+    private score: number = 0;
 
     start() {
         this.schedule(this.spawnFood, this.foodSpawnInterval);
+        this.scoreLabel.string = this.score.toString();
     }
 
+    //TODO: set so that it only spawns untill a max of x on screen at once
     spawnFood() {
-        let food:Node = instantiate(this.foodPrefab);
-        food.setParent(this.node);
+        //stops if maxFood is reached
+        //maxFood + 1 because there is an invisible food at the start 
+        if (this.currFood >= this.maxFood + 1) return;
 
-        let randX = Math.random() * view.getVisibleSize().width;
-        let randY = Math.random() * view.getVisibleSize().height;
+        let food: Node = instantiate(this.foodPrefab);
+        food.setParent(this.foodContainer);
 
-        food.setWorldPosition(randX, randY, 0);
+        let width = view.getVisibleSize().width;
+        let height = view.getVisibleSize().height;
+
+        let randX = (Math.random() - 0.5) * width;
+        let randY = (Math.random() - 0.5) * height;
+
+        food.setPosition(randX, randY, 0);
+        this.currFood++;
+    }
+
+    onFoodEaten(foodNode: Node) {
+
+        foodNode.removeFromParent();
+        foodNode.destroy();
+
+        this.currFood--;
+
+        this.score += 10;
+        this.updateScoreUI();
+    }
+
+    updateScoreUI() {
+        this.scoreLabel.string = "" + this.score;
+    }
+
+    onGhostEaten(EnemyNode: Node) {
+        //this.enemy.respawn or something
+        this.score += 200;
+        this.updateScoreUI;
     }
 
     update(deltaTime: number) {
-        
+
     }
 }
 
--- assets/scripts/Player.ts
+++ assets/scripts/Player.ts
@@ -1,18 +1,30 @@
-import { _decorator, Component, EventTouch, Input, input, Node, Vec3, Vec2, RigidBody2D, Collider, Contact2DType, Collider2D, IPhysics2DContact } from 'cc';
+import { _decorator, Component, EventTouch, Input, input, Node, Vec3, Vec2, RigidBody2D, Collider, Contact2DType, Collider2D, IPhysics2DContact, Label, view } from 'cc';
 import { Enemy } from './Enemy';
+import { Heart } from './Heart';
+import { Food } from './Food';
+import { GameController } from './GameController';
 const { ccclass, property } = _decorator;
 
 @ccclass('Player')
 export class Player extends Component {
 
-    @property
-    private baseSpeed:number = 3;
 
-    private touchPos:Vec3 = new Vec3();
-    public state:string = "idle";
-    private health:number = 3;
+    @property({ type: Heart })
+    private heart: Heart = null;
+
+    @property(GameController)
+    private gameController: GameController;
+
+    @property
+    private baseSpeed: number = 3;
+
+    private touchPos: Vec3 = new Vec3();
+    public state: string = "idle";
+    public health: number = 3;
+
 
     start() {
+        //for clicking and dragging input
         input.on(Input.EventType.TOUCH_START, this.onTouchStart, this);
         input.on(Input.EventType.TOUCH_MOVE, this.onTouchStart, this);
         input.on(Input.EventType.TOUCH_END, this.onTouchEnd, this);
@@ -20,15 +32,28 @@ export class Player extends Component {
         this.touchPos = this.node.getWorldPosition().clone();
 
         this.node.getComponent(Collider2D).on(Contact2DType.BEGIN_CONTACT, this.onStartContact, this);
+
+        //set score
+
     }
+
 
     onStartContact(selfCollider: Collider2D, otherCollider: Collider2D, contact: IPhysics2DContact | null) {
         if (otherCollider.node.getComponent(Enemy)) {
-            this.health--;
-            if (this.health <= 0) {
-                this.changeState("die");
+            if (otherCollider.node.getComponent(Enemy).state == "vulnerable") {
+                //TODO: Kill enemy then respawn
+                //GameController.enemyEaten();
+            } else {
+                this.health--;
+                this.heart.loseLife();
+                //TODO: pacman respawn code here
+
+                if (this.health <= 0) {
+                    this.changeState("die");
+                }
             }
         }
+
     }
 
     changeState(nextState) {
@@ -48,27 +73,52 @@ export class Player extends Component {
         }
     }
 
-    onTouchStart(event:EventTouch) {
-        this.touchPos = new Vec3(event.getUILocation().x, event.getUILocation().y, 0);
+    //click and drag to move
+    onTouchStart(event: EventTouch) {
+        let uiPos = event.getUILocation();
+
+        let width = view.getVisibleSize().width;
+        let height = view.getVisibleSize().height;
+
+        this.touchPos = new Vec3(
+            uiPos.x - width / 2,
+            uiPos.y - height / 2,
+            0
+        );
+
         this.changeState("move");
     }
 
-    onTouchEnd(event:EventTouch) {
+    //after finished dragging, reset back to idle
+    onTouchEnd(event: EventTouch) {
         this.touchPos = new Vec3(event.getUILocation().x, event.getUILocation().y, 0);
         this.changeState("idle");
     }
 
+
+
+
+
     update(deltaTime: number) {
         if (this.state == "move") {
-            let deltaPos = this.touchPos.clone().subtract(this.node.getWorldPosition());
+            let deltaPos = this.touchPos.clone().subtract(this.node.position);
             let hypotenuse = deltaPos.length();
-            let speedX = deltaPos.x/hypotenuse;
-            let speedY = deltaPos.y/hypotenuse;
+            let speedX = deltaPos.x / hypotenuse;
+            let speedY = deltaPos.y / hypotenuse;
             let speedVector = new Vec2(speedX, speedY);
             speedVector = speedVector.multiplyScalar(this.baseSpeed);
             this.node.getComponent(RigidBody2D).linearVelocity = speedVector;
         }
+
+        for (let food of this.gameController.foodContainer.children) {
+            if (!food.active) continue;
+
+            let dist = Vec3.distance(this.node.position, food.position);
+
+            if (dist < 30) {
+                food.active = false; // prevent multiple triggers
+                this.gameController.onFoodEaten(food);
+            }
+        }
     }
 }
-
-
