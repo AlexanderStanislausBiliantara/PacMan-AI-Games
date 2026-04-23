cea3c5ddd884878843c9e786710dcce4caed08df
Yohan Leonardo <yohanleonardo150@gmail.com>
2026-04-23 23:36:55 +0700

    power ups + respawn ghost

--- assets/scripts/Enemy.ts
+++ assets/scripts/Enemy.ts
@@ -1,80 +1,114 @@
-import { _decorator, Component, Node, RigidBody2D, Vec2, math, Vec3 } from 'cc';
-import { Player } from './Player';
-const { ccclass, property } = _decorator;
+import {
+    _decorator,
+    Component,
+    Node,
+    RigidBody2D,
+    Vec2,
+    Vec3,
+    Sprite,
+    Color,
+} from 'cc'
+import { Player } from './Player'
+const { ccclass, property } = _decorator
 
 @ccclass('Enemy')
 export class Enemy extends Component {
-
-    @property({type:Node})
-    private player:Node
+    @property({ type: Node })
+    private player: Node
 
     @property
-    private baseSpeed:number = 1;
+    private baseSpeed: number = 1
 
-    public state:string = "idle";
-    private wanderTarget:Vec2 = new Vec2();
-    private timer:number = 0;
-    private isChasing:boolean = false;
+    public state: string = 'idle'
+    private wanderTarget: Vec2 = new Vec2()
+    private timer: number = 0
+    private isChasing: boolean = false
+    private vulnerableTimer: number = 0
+    private startPos: Vec3 = new Vec3()
+    private originalColor: Color = new Color(255, 255, 255, 255)
 
     start() {
+        this.startPos = this.node.position.clone()
+        const sprite = this.getComponent(Sprite)
+        if (sprite) {
+            this.originalColor = sprite.color.clone()
+        }
+    }
 
+    setVulnerable(active: boolean) {
+        const sprite = this.getComponent(Sprite)
+        if (active) {
+            this.state = 'vulnerable'
+            this.vulnerableTimer = 15
+            if (sprite) {
+                sprite.color = new Color(0, 0, 255, 255)
+            }
+        } else {
+            this.state = 'idle'
+            if (sprite) {
+                sprite.color = this.originalColor
+            }
+        }
+    }
+
+    respawn() {
+        this.node.setPosition(this.startPos)
+        this.setVulnerable(false)
+
+        const rb = this.getComponent(RigidBody2D)
+        if (rb) rb.linearVelocity = Vec2.ZERO
     }
 
     update(deltaTime: number) {
-        let playerDist = Vec3.distance(this.node.position, this.player.position);
-        let rb = this.getComponent(RigidBody2D);
-        let finalVelocity = new Vec2();
+        if (this.state == 'vulnerable') {
+            this.vulnerableTimer -= deltaTime
+            if (this.vulnerableTimer <= 0) {
+                this.setVulnerable(false)
+            }
+        }
 
-        if (this.player.getComponent(Player).state == "move" && playerDist <= 150) {
-            this.isChasing = true;
-            let dir = new Vec2();
-            dir.x = this.player.position.x - this.node.position.x;
-            dir.y = this.player.position.y - this.node.position.y;
-            dir.normalize().multiplyScalar(this.baseSpeed);
-            finalVelocity = dir;
-            // let deltaPos = this.player.position.clone().subtract(this.node.position);
-            // let hypotenuse = deltaPos.length();
-            // let speedX = deltaPos.x/hypotenuse;
-            // let speedY = deltaPos.y/hypotenuse;
-            // let speedVector = new Vec2(speedX, speedY);
-            // speedVector = speedVector.multiplyScalar(this.baseSpeed);
-            // this.node.getComponent(RigidBody2D).linearVelocity = speedVector;
+        if (!this.player || !this.player.isValid) return
+
+        let playerDist = Vec3.distance(this.node.position, this.player.position)
+        let rb = this.getComponent(RigidBody2D)
+        if (!rb) return
+
+        let finalVelocity = new Vec2()
+
+        if (this.state == 'vulnerable') {
+            let dir = new Vec2()
+            dir.x = this.node.position.x - this.player.position.x
+            dir.y = this.node.position.y - this.player.position.y
+            dir.normalize().multiplyScalar(this.baseSpeed * 0.5)
+            finalVelocity = dir
+        } else if (
+            this.player.getComponent(Player) &&
+            this.player.getComponent(Player).state == 'move' &&
+            playerDist <= 200
+        ) {
+            this.isChasing = true
+            let dir = new Vec2()
+            dir.x = this.player.position.x - this.node.position.x
+            dir.y = this.player.position.y - this.node.position.y
+            dir.normalize().multiplyScalar(this.baseSpeed)
+            finalVelocity = dir
         } else {
-            this.isChasing = false;
-            this.timer -= deltaTime;
+            this.isChasing = false
+            this.timer -= deltaTime
 
             if (this.timer <= 0) {
-                this.wanderTarget.x = (Math.random() * 500) - 250;
-                this.wanderTarget.y = (Math.random() * 500) - 250;
-                this.timer = 6;
+                this.wanderTarget.x = Math.random() * 500 - 250
+                this.wanderTarget.y = Math.random() * 500 - 250
+                this.timer = 6
             }
 
-            let dir = new Vec2();
-            dir.x = this.wanderTarget.x - this.node.position.x;
-            dir.y = this.wanderTarget.y - this.node.position.y;
-            dir.normalize().multiplyScalar(this.baseSpeed);
-            finalVelocity = dir;
-            // let direction:Vec3 = new Vec3();
-            // direction.x = Math.random() * 2 - 1;
-            // direction.y = Math.random() * 2 - 1;
-            // direction.z = 0;
-
-            // let deltaPos = direction.clone().subtract(this.node.position);
-            // let hypotenuse = deltaPos.length();
-            // let speedX = deltaPos.x/hypotenuse;
-            // let speedY = deltaPos.y/hypotenuse;
-            // let speedVector = new Vec2(speedX, speedY);
-            // speedVector = speedVector.multiplyScalar(this.baseSpeed);
-            // this.node.getComponent(RigidBody2D).linearVelocity = speedVector;
+            let dir = new Vec2()
+            dir.x = this.wanderTarget.x - this.node.position.x
+            dir.y = this.wanderTarget.y - this.node.position.y
+            dir.normalize().multiplyScalar(this.baseSpeed)
+            finalVelocity = dir
         }
 
-        if (this.state == "die") {
-            this.node.destroy();
-        }
-
-        rb.linearVelocity = finalVelocity;
-        //this.node.translate(deltaPos.multiplyScalar(deltaTime));
+        rb.linearVelocity = finalVelocity
     }
 }
-
-
--- assets/scripts/Player.ts
+++ assets/scripts/Player.ts
@@ -1,123 +1,190 @@
-import { _decorator, Component, EventTouch, Input, input, Node, Vec3, Vec2, RigidBody2D, Collider, Contact2DType, Collider2D, IPhysics2DContact, Label, view } from 'cc';
-import { Enemy } from './Enemy';
-import { Heart } from './Heart';
-import { Food } from './Food';
-import { GameController } from './GameController';
-const { ccclass, property } = _decorator;
+import {
+    _decorator,
+    Component,
+    EventTouch,
+    Input,
+    input,
+    Node,
+    Vec3,
+    Vec2,
+    RigidBody2D,
+    view,
+    Sprite,
+    Color,
+    tween,
+    director,
+} from 'cc'
+import { Enemy } from './Enemy'
+import { Heart } from './Heart'
+import { PowerUp } from './PowerUp'
+import { GameController } from './GameController'
+const { ccclass, property } = _decorator
 
 @ccclass('Player')
 export class Player extends Component {
-
-
     @property({ type: Heart })
-    private heart: Heart = null;
+    private heart: Heart = null
 
     @property(GameController)
-    private gameController: GameController;
+    private gameController: GameController
 
     @property
-    private baseSpeed: number = 3;
+    private baseSpeed: number = 3
 
-    private touchPos: Vec3 = new Vec3();
-    public state: string = "idle";
-    public health: number = 3;
+    @property(Color)
+    private powerUpColor: Color = new Color(255, 255, 0, 255)
 
-
+    private originalColor: Color = new Color(255, 255, 255, 255)
+    private touchPos: Vec3 = new Vec3()
+    public state: string = 'idle'
+    public health: number = 3
+    private isPoweredUp: boolean = false
+    private isInvincible: boolean = false
+    private currentDir: Vec2 = new Vec2(1, 0)
     start() {
-        //for clicking and dragging input
-        input.on(Input.EventType.TOUCH_START, this.onTouchStart, this);
-        input.on(Input.EventType.TOUCH_MOVE, this.onTouchStart, this);
-        input.on(Input.EventType.TOUCH_END, this.onTouchEnd, this);
+        input.on(Input.EventType.TOUCH_START, this.onTouchStart, this)
 
-        this.touchPos = this.node.getWorldPosition().clone();
+        this.touchPos = this.node.getWorldPosition().clone()
 
-        this.node.getComponent(Collider2D).on(Contact2DType.BEGIN_CONTACT, this.onStartContact, this);
-
-        //set score
-
-    }
-
-
-    onStartContact(selfCollider: Collider2D, otherCollider: Collider2D, contact: IPhysics2DContact | null) {
-        if (otherCollider.node.getComponent(Enemy)) {
-            if (otherCollider.node.getComponent(Enemy).state == "vulnerable") {
-                //TODO: Kill enemy then respawn
-                //GameController.enemyEaten();
-            } else {
-                this.health--;
-                this.heart.loseLife();
-                //TODO: pacman respawn code here
-
-                if (this.health <= 0) {
-                    this.changeState("die");
-                }
-            }
+        const sprite = this.getComponent(Sprite)
+        if (sprite) {
+            this.originalColor = sprite.color.clone()
         }
 
+        const rb = this.getComponent(RigidBody2D)
+        if (rb) {
+            rb.enabledContactListener = false
+        }
+
+        this.state = 'move'
+    }
+
+    setPowerUpEffect(active: boolean) {
+        this.isPoweredUp = active
+        this.updateVisualEffect()
+    }
+
+    private updateVisualEffect() {
+        const sprite = this.getComponent(Sprite)
+        if (!sprite || this.isInvincible) return
+        sprite.color = this.isPoweredUp ? this.powerUpColor : this.originalColor
+    }
+
+    takeDamage() {
+        if (this.isInvincible || this.state == 'die') return
+
+        this.health--
+        if (this.heart) this.heart.loseLife()
+
+        if (this.health <= 0) {
+            this.changeState('die')
+            return
+        }
+
+        this.isInvincible = true
+        const sprite = this.getComponent(Sprite)
+        if (sprite) {
+            tween(sprite)
+                .to(0.1, { color: new Color(255, 255, 255, 0) })
+                .to(0.1, { color: this.originalColor })
+                .union()
+                .repeat(10)
+                .call(() => {
+                    this.isInvincible = false
+                    this.updateVisualEffect()
+                })
+                .start()
+        } else {
+            this.scheduleOnce(() => {
+                this.isInvincible = false
+            }, 2)
+        }
     }
 
     changeState(nextState) {
-        if (nextState == "die") {
-            this.state = nextState;
-            return;
+        if (nextState == 'die') {
+            this.state = nextState
+            input.off(Input.EventType.TOUCH_START, this.onTouchStart, this)
+            const rb = this.getComponent(RigidBody2D)
+            if (rb) rb.linearVelocity = Vec2.ZERO
         } else {
-            if (this.state == "idle") {
-                if (nextState == "move") {
-                    this.state = nextState;
-                }
-            } else if (this.state == "move") {
-                if (nextState == "idle") {
-                    this.state = nextState;
+            this.state = nextState
+        }
+    }
+
+    onTouchStart(event: EventTouch) {
+        if (this.state == 'die') return
+        let uiPos = event.getUILocation()
+        let width = view.getVisibleSize().width
+        let height = view.getVisibleSize().height
+
+        this.touchPos = new Vec3(uiPos.x - width / 2, uiPos.y - height / 2, 0)
+
+        let deltaPos = this.touchPos.clone().subtract(this.node.position)
+        if (deltaPos.length() > 0) {
+            this.currentDir = new Vec2(deltaPos.x, deltaPos.y).normalize()
+        }
+
+        this.changeState('move')
+    }
+
+    update(deltaTime: number) {
+        if (this.state == 'move') {
+            const rb = this.getComponent(RigidBody2D)
+            if (rb) {
+                rb.linearVelocity = this.currentDir
+                    .clone()
+                    .multiplyScalar(this.baseSpeed)
+            }
+        } else if (this.state == 'idle') {
+            const rb = this.getComponent(RigidBody2D)
+            if (rb) rb.linearVelocity = Vec2.ZERO
+        }
+
+        const myPos = this.node.worldPosition
+
+        if (this.gameController && this.gameController.foodContainer) {
+            for (let food of this.gameController.foodContainer.children) {
+                if (!food.active) continue
+                if (Vec3.distance(myPos, food.worldPosition) < 40) {
+                    this.gameController.onFoodEaten(food)
                 }
             }
         }
-    }
 
-    //click and drag to move
-    onTouchStart(event: EventTouch) {
-        let uiPos = event.getUILocation();
-
-        let width = view.getVisibleSize().width;
-        let height = view.getVisibleSize().height;
-
-        this.touchPos = new Vec3(
-            uiPos.x - width / 2,
-            uiPos.y - height / 2,
-            0
-        );
-
-        this.changeState("move");
-    }
-
-    //after finished dragging, reset back to idle
-    onTouchEnd(event: EventTouch) {
-        this.touchPos = new Vec3(event.getUILocation().x, event.getUILocation().y, 0);
-        this.changeState("idle");
-    }
-
-
-
-
-
-    update(deltaTime: number) {
-        if (this.state == "move") {
-            let deltaPos = this.touchPos.clone().subtract(this.node.position);
-            let hypotenuse = deltaPos.length();
-            let speedX = deltaPos.x / hypotenuse;
-            let speedY = deltaPos.y / hypotenuse;
-            let speedVector = new Vec2(speedX, speedY);
-            speedVector = speedVector.multiplyScalar(this.baseSpeed);
-            this.node.getComponent(RigidBody2D).linearVelocity = speedVector;
+        const powerUpContainer = this.gameController
+            ? this.gameController.powerUpContainer ||
+              this.gameController.foodContainer
+            : null
+        if (powerUpContainer) {
+            for (let powerUp of powerUpContainer.children) {
+                if (!powerUp.active) continue
+                if (
+                    powerUp.getComponent(PowerUp) ||
+                    powerUp.name.toLowerCase().includes('powerup')
+                ) {
+                    if (Vec3.distance(myPos, powerUp.worldPosition) < 50) {
+                        this.gameController.onPowerUpEaten(powerUp)
+                    }
+                }
+            }
         }
 
-        for (let food of this.gameController.foodContainer.children) {
-            if (!food.active) continue;
+        const scene = director.getScene()
+        if (scene) {
+            const enemies = scene.getComponentsInChildren(Enemy)
+            for (let enemy of enemies) {
+                if (!enemy.node.active) continue
 
-            let dist = Vec3.distance(this.node.position, food.position);
+                let dist = Vec3.distance(myPos, enemy.node.worldPosition)
 
-            if (dist < 30) {
-                food.active = false; // prevent multiple triggers
-                this.gameController.onFoodEaten(food);
+                if (dist < 80) {
+                    if (enemy.state === 'vulnerable') {
+                        this.gameController.onGhostEaten(enemy.node)
+                    } else {
+                        this.takeDamage()
+                    }
+                }
             }
         }
     }
